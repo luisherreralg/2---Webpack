319c729c1f89d88cf2e8f3c686ee3f97bfc99b02
luisherreralg <lherreralg@outlook.es>
2022-12-19 10:52:07 +0100

    🚧 Cameras and controls

--- src/script.js
+++ src/script.js
@@ -1,10 +1,27 @@
 import "./style.css";
 import * as THREE from "three";
-import gsap from "gsap";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
+/**
+ * * CURSOR
+ */
+const cursor = {
+  x: 0,
+  y: 0,
+};
+window.addEventListener("mousemove", (event) => {
+  cursor.x = event.clientX / sizes.width - 0.5;
+  // Hay que invertir el valor del cursor.y
+  // Ya que cuando el cursor baja es positivo
+  // Mientras que en three.js el eje y es positivo cuando sube
+  // Resumen: Que el cursor.y y three.js funcionan diferente y haq que hacer que se entiendan
+  cursor.y = -(event.clientY / sizes.height - 0.5);
+});
 
 /**
  * * -------------------- SCENE --------------------
  */
+const canvas = document.querySelector(".webgl");
 const scene = new THREE.Scene();
 
 /**
@@ -25,27 +42,27 @@ const cube1 = new THREE.Mesh(
 );
 group.add(cube1);
 
-const cube2 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({
-    color: 0x00ff00,
-  })
-);
-cube2.position.x = -2;
-group.add(cube2);
+// const cube2 = new THREE.Mesh(
+//   new THREE.BoxGeometry(1, 1, 1),
+//   new THREE.MeshBasicMaterial({
+//     color: 0x00ff00,
+//   })
+// );
+// cube2.position.x = -2;
+// group.add(cube2);
 
-const cube3 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({
-    color: 0x0000ff,
-  })
-);
-cube3.position.x = 2;
-group.add(cube3);
+// const cube3 = new THREE.Mesh(
+//   new THREE.BoxGeometry(1, 1, 1),
+//   new THREE.MeshBasicMaterial({
+//     color: 0x0000ff,
+//   })
+// );
+// cube3.position.x = 2;
+// group.add(cube3);
 
 // Axis Helper (para poder ver los ejes)
-const axesHelper = new THREE.AxesHelper();
-scene.add(axesHelper);
+// const axesHelper = new THREE.AxesHelper();
+// scene.add(axesHelper);
 
 /**
  * * -------------------- SIZES --------------------
@@ -60,14 +77,37 @@ const sizes = {
  */
 const fov = 75;
 const aspectRatio = sizes.width / sizes.height;
-const camera = new THREE.PerspectiveCamera(fov, aspectRatio);
+const near = 0.1;
+const far = 100;
+const camera = new THREE.PerspectiveCamera(fov, aspectRatio, near, far);
+
+// Ortographic camera settings
+// const left = -1 * aspectRatio;
+// const right = 1 * aspectRatio;
+// const top = 1;
+// const bottom = -1;
+// const camera = new THREE.OrthographicCamera(
+//   left,
+//   right,
+//   top,
+//   bottom,
+//   near,
+//   far
+// );
+
 camera.position.z = 3;
+// camera.position.y = 2;
+// camera.position.x = 2;
+camera.lookAt(group.position);
 scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
 /**
  * * -------------------- RENDERER --------------------
  */
-const canvas = document.querySelector(".webgl");
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 });
@@ -79,29 +119,26 @@ renderer.setSize(sizes.width, sizes.height);
  * * -------------------- ANIMATIONS --------------------
  */
 
-gsap.to(group.position, {
-  duration: 1,
-  delay: 1,
-  x: 2,
-});
-gsap.to(group.position, {
-  duration: 1,
-  delay: 2,
-  x: 0,
-});
-
-// Clock
-// const clock = new THREE.Clock();
+// Clock;
+const clock = new THREE.Clock();
 
 const tick = () => {
-  //   // Clock
-  //   const elapsedTime = clock.getElapsedTime();
+  // Clock
+  const elapsedTime = clock.getElapsedTime();
 
-  //   // Update Object
-  //   group.rotation.x = elapsedTime * Math.PI * 2;
-  //   group.rotation.y = elapsedTime * Math.PI * 2;
-  //   group.position.y = Math.sin(elapsedTime);
-  //   group.position.x = Math.cos(elapsedTime);
+  // // Update Camera
+  // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
+  // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
+  // camera.position.y = cursor.y * 5;
+
+  // camera.lookAt(new THREE.Vector3()) // Centro de coordenadas 0,0,0
+  camera.lookAt(group.position);
+
+  // Update Object
+  // group.rotation.y = elapsedTime * Math.PI * 0.4;
+
+  // Update Controls
+  controls.update();
 
   // Render
   renderer.render(scene, camera);
